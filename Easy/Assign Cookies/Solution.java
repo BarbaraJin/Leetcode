c6ee5f958414e3c11e5a8ff7f58254f7d92743d7
BarbaraJin <j_meixiang@outlook.com>
2018-07-10 22:51:47 -0400

    #455-1
    nlogn

--- Easy/Assign Cookies/Solution.java
+++ Easy/Assign Cookies/Solution.java
@@ -12,32 +12,21 @@ Output: 1
 Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
 And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
 You need to output 1.*/
+import java.util.Arrays;
 
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
     	int answer = 0;
-    	boolean find = false;
-        for (int each : g) {
-        	int min = each;
-        	for (int i=0;i<s.length;i++ ) {
-        		if(s[i]>=each){
-        			if(!find){
-        				min = i;
-        				find = true;
-        			}
-        			else{
-        				if(s[i]<=s[min]){
-        					min = i;
-        				}
-        			}
-        		}
-        	}
-        	if(find){
-        		answer++;
-        		find = false;
-        		s[min]=-1;
-        	}
-        }
+    	Arrays.sort(g);
+    	Arrays.sort(s);
+
+    	int i=0;
+    	for (int j=0; i<g.length&&j<s.length; j++) {    		
+    		if(s[j]>=g[i]){
+    			answer++;
+    			i++;
+    		}
+    	}
         return answer;
     }
 }
