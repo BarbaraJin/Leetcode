ea51ff529254b50f63f075506da1e0462dfa44c6
BarbaraJin <j_meixiang@outlook.com>
2018-09-26 21:40:05 -0400

    #167

---
+++ Easy/two sum II/Solution.java
@@ -0,0 +1,50 @@
+/* Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+Example:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2. */
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] answers = new int[2];
+
+        int ind1=0,ind2= numbers.length-1;
+        //TODO: while condition
+        while (true) {
+            int secondInt = target-numbers[ind1];
+            if (secondInt<numbers[ind2]) {
+                ind2--;
+            }
+            else if(secondInt == numbers[ind2]){
+                break;
+            }
+            else{
+                ind1++;
+            }
+        }
+        answers[0]=ind1+1;
+        answers[1]=ind2+1;
+        if(ind1==ind2){
+            return null;
+        }
+        return answers;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2,7,11,150};
+        int target = 9;
+        int[] answer = solution.twoSum(nums, target);
+        for (int var : answer) {
+            System.out.print(var+" ");
+        }
+    }
+}
